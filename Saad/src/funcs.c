b084a8aad44e82285e971b6ded978a04f90446fc
saidriss <113543660+s2adr@users.noreply.github.com>
2024-09-28 14:36:10 +0200

    Create funcs.c

---
+++ Saad/src/funcs.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   funcs.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saidriss <saidriss@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 22:17:39 by saidriss          #+#    #+#             */
+/*   Updated: 2024/09/28 13:18:22 by saidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+long long int	ft_atoi(char *str)
+{
+	long long int	res;
+	int	sign;
+
+	sign = 1;
+	res = 0;
+	while (*str && ((*str == ' ' || (*str >= '\t' && *str <= '\r'))))
+		++str;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		++str;
+	}
+	if (*str < '0' || *str > '9')
+		return (0);
+	else
+	{
+		while (*str && *str >= '0' && *str <= '9')
+		{
+			res = res * 10 + (*str - 48);
+			++str;
+		}
+		return (res * sign);
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
