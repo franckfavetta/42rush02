ceab940e8df2018376c56d31d80ac267e4eb1047
saidriss <113543660+s2adr@users.noreply.github.com>
2024-09-28 22:06:23 +0200

    Update main.c

--- Saad/src/main.c
+++ Saad/src/main.c
@@ -15,33 +15,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void	ft_print(int n, t_list *tab, int *first)
-{
-	int	i;
-	int	mult;
-
-	i = 0;
-	mult = getmult(n);
-	if (mult >= 100)
-		ft_print(n / mult, tab, first);
-	if (*first == 0)
-		write(1, " ", 1);
-	*first = 0;
-	while (tab[i].nb != mult)
-		i++;
-	ft_putstr(tab[i].val);
-	if (mult != 0 && n % mult != 0)
-		ft_print(n % mult, tab, first);
-}
-
 int	main(int ac, char **av)
 {
 	t_list	*tab;
 	int		*first;
 	int		addr_first;
 
-	addr_first = 1;
-	first = &addr_first;
 	if (ac == 2)
 	{
 		if (ft_atoi(av[1]) == 0)
