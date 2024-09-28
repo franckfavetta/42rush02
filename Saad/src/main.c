2e6fd6bd5b636983352afda8740a2294aeaddab9
saidriss <113543660+s2adr@users.noreply.github.com>
2024-09-28 22:04:34 +0200

    Update main.c

--- Saad/src/main.c
+++ Saad/src/main.c
@@ -6,7 +6,7 @@
 /*   By: saidriss <saidriss@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 22:15:45 by saidriss          #+#    #+#             */
-/*   Updated: 2024/09/28 14:15:13 by saidriss         ###   ########.fr       */
+/*   Updated: 2024/09/28 22:05:13 by saidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-
-int	getdec(int nb)
-{
-	if (nb >= 90)
-		return (90);
-	else if (nb >= 80)
-		return (80);
-	else if (nb >= 70)
-		return (70);
-	else if (nb >= 60)
-		return (60);
-	else if (nb >= 50)
-		return (50);
-	else if (nb >= 40)
-		return (40);
-	else if (nb >= 30)
-		return (30);
-	else if (nb >= 20)
-		return (20);
-	else if (nb <= 20)
-		return (nb);
-	else
-		return (0);
-}
-
-int	getmult(int nb)
-{
-	if (nb >= 1000000000)
-		return (1000000000);
-	else if (nb >= 1000000)
-		return (1000000);
-	else if (nb >= 1000)
-		return (1000);
-	else if (nb >= 100)
-		return (100);
-	else
-		return (getdec(nb));
-}
 
 void	ft_print(int n, t_list *tab, int *first)
 {
