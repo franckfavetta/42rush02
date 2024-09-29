5f8285bacf1e08b732ea5296043861cd3fbbc3e8
saidriss <113543660+s2adr@users.noreply.github.com>
2024-09-29 10:31:18 +0200

    update all src files

---
+++ Saad/src/main.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saidriss <saidriss@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 22:15:45 by saidriss          #+#    #+#             */
+/*   Updated: 2024/09/29 09:28:19 by saidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int	getdec(int nb)
+{
+	if (nb >= 90)
+		return (90);
+	else if (nb >= 80)
+		return (80);
+	else if (nb >= 70)
+		return (70);
+	else if (nb >= 60)
+		return (60);
+	else if (nb >= 50)
+		return (50);
+	else if (nb >= 40)
+		return (40);
+	else if (nb >= 30)
+		return (30);
+	else if (nb >= 20)
+		return (20);
+	else if (nb <= 20)
+		return (nb);
+	else
+		return (0);
+}
+
+long long int	getmult(long long int nb)
+{
+	if (nb >= 1000000000)
+		return (1000000000);
+	else if (nb >= 1000000)
+		return (1000000);
+	else if (nb >= 1000)
+		return (1000);
+	else if (nb >= 100)
+		return (100);
+	else
+		return (getdec(nb));
+}
+
+void	ft_print(long long int n, t_list *tab, int *first)
+{
+	int				i;
+	long long int	mult;
+
+	i = 0;
+	mult = getmult(n);
+	if (mult >= 100)
+		ft_print(n / mult, tab, first);
+	if (*first == 0)
+		write(1, " ", 1);
+	*first = 0;
+	while (tab[i].nb != mult)
+		i++;
+	ft_putstr(tab[i].val);
+	if (mult != 0 && n % mult != 0)
+		ft_print(n % mult, tab, first);
+}
+
+int	ft_case(int nb)
+{
+	if (nb == 1)
+	{
+		write(2, "Error", 5);
+		return (0);
+	}
+	else if (nb == 2)
+	{
+		write(1, "zero", 4);
+		return (0);
+	}
+	else
+		return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_list	*tab;
+	int		*first;
+	int		addr;
+
+	addr = 1;
+	first = &addr;
+	if (ac == 2)
+	{
+		if (ft_atoi(av[1]) < 0)
+			ft_case(1);
+		else if (ft_atoi(av[1]) == 0)
+			ft_case(2);
+		else
+		{
+			tab = extract("numbers.dict");
+			ft_print(ft_atoi(av[1]), tab, first);
+		}
+	}
+	else if (ac == 3)
+	{
+		tab = extract(av[1]);
+		ft_print(ft_atoi(av[2]), tab, first);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
---
+++ Saad/src/rush02.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saidriss <saidriss@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/27 22:14:33 by saidriss          #+#    #+#             */
+/*   Updated: 2024/09/29 09:27:30 by saidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char	*ft_getval(int fd, char *c)
+{
+	int		i;
+	char	*str;
+
+	str = malloc(sizeof(char) * 128);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (c[0] != '\n')
+	{
+		str[i++] = c[0];
+		read(fd, c, 1);
+	}
+	return (str);
+}
+
+char	*ft_getnb(int fd)
+{
+	int		i;
+	char	c[1];
+	char	*str;
+
+	str = malloc(sizeof(char) * 128);
+	if (!str)
+		return (NULL);
+	i = 0;
+	read(fd, c, 1);
+	while (c[0] == '\n')
+		read(fd, c, 1);
+	while (c[0] >= '0' && c[0] <= '9')
+	{
+		str[i++] = c[0];
+		read(fd, c, 1);
+	}
+	return (str);
+}
+
+void	read_space(int fd, char *c)
+{
+	read(fd, c, 1);
+	while (c[0] == ' ')
+		read(fd, c, 1);
+	if (c[0] == ':')
+		read(fd, c, 1);
+	while (c[0] == ' ')
+		read(fd, c, 1);
+}
+
+t_list	*extract(char *file)
+{
+	int		i;
+	int		fd;
+	t_list	*tab;
+	char	c[1];
+
+	fd = open(file, O_RDONLY);
+	tab = malloc(sizeof(t_list) * 41);
+	if (fd == -1 || (!tab))
+		return (NULL);
+	i = 0;
+	while (i++ < 40)
+	{
+		tab[i].nb = ft_atoi(ft_getnb(fd));
+		read_space(fd, c);
+		tab[i].val = ft_getval(fd, c);
+		tab[i].val = ft_strdup(tab[i].val);
+	}
+	close(fd);
+	return (tab);
+}
